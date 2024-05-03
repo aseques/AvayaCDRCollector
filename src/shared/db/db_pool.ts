76c61bc16987f0bca9663c744a7d6a031f3a94ff
Joan <joan.juvanteny@bassolsenergia.com>
2024-05-03 15:00:49 +0200

    Allow custom db name and fix connection to mysql

--- src/shared/db/db_pool.ts
+++ src/shared/db/db_pool.ts
@@ -2,8 +2,9 @@ import mariadb from 'mariadb';
 
 export const dbPool = mariadb.createPool({
   host: process.env.DB_HOST,
-  database: 'itsupport',
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   connectionLimit: 5,
+  allowPublicKeyRetrieval: true,
 });
